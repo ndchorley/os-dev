0009457897e307120570137ea9ca8d01dbeeab98
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-03-14 15:17:06 +0000

    Introduce a FLAGS variable

--- boot.s
+++ boot.s
@@ -1,8 +1,9 @@
 	.set MAGIC, 0x1BADB002
-	.set CHECKSUM, -MAGIC
+	.set FLAGS, 74 // Just to check a value that isn't 0
+	.set CHECKSUM, -(MAGIC + FLAGS)
 
 	.section .multiboot
 	.align 4
 	.long MAGIC
-	.long 74 // Just to check a value that isn't 0.
+	.long FLAGS
 	.long CHECKSUM

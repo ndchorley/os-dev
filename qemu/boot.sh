6d893066115b85704a55f69be2fe82c9d8023cf1
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-03-27 11:09:22 +0000

    Comment out options for booting from CD ISO

--- qemu/boot.sh
+++ qemu/boot.sh
@@ -2,6 +2,6 @@
 
 qemu-system-x86_64 \
     -m 4G \
-    -boot d \
-    -cdrom debian-13.4.0-amd64-netinst.iso \
+#    -boot d \
+#    -cdrom debian-13.4.0-amd64-netinst.iso \
     -drive file=disk.qcow2,format=qcow2 \

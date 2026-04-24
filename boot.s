54b3fe8235c959bb8f5165352dd3480b9cd81fc9
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-04-24 11:40:49 +0100

    Add _start, which will be where the bootloader jumps to

--- boot.s
+++ boot.s
@@ -15,3 +15,15 @@
 stack_bottom:
 	.skip 16384
 stack_top:
+
+	.section .text
+	.global _start
+	.type _start, @function
+_start:
+	mov $stack_top, %esp
+
+	call kernel_main
+
+	cli
+1:	hlt
+	jmp 1b

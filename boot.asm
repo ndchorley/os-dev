bff18938bd14fbed5bf254f4a0aaa7f56731da80
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-07-10 07:55:04 +0100

    Use service 2 to move the cursor to the next line

--- boot.asm
+++ boot.asm
@@ -11,6 +11,17 @@
 	mov cx, 0x14 ; Number of times to write
 	int 0x10
 
+	mov ah, 0x2 ; Service 2: set cursor position
+	mov bh, 0x0 ; Display page
+	mov dh, 0x1 ; Row
+	mov dl, 0x0 ; Column
+	int 0x10
+
+	mov ah, 0xA
+	mov al, 0x2A
+	mov cx, 0x3
+	int 0x10
+
 	times 510-($-$$) db 0
 	db 0x55
 	db 0xAA
